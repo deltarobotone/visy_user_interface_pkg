652fc2d8be6858c44284f9761e3b5637020a98b6
buerger-pascal <buerger.pascal@web.de>
2020-08-31 15:21:49 +0200

    modify update

--- src/main_window.cpp
+++ src/main_window.cpp
@@ -201,7 +201,7 @@ void MainWindow::updateButtonHandle()
       this->hide();
       process->waitForFinished(-1);
       process->close();
-      reply = QMessageBox::question(this, "Update", "Please reboot the system");
+      reply = QMessageBox::information(this, "Update", "Please reboot the system");
       this->close();
     }
     catch(const std::exception& e){
