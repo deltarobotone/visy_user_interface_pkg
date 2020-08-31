6e154c4a0a2cd8f92f670d30d8802358092ef6dd
buerger-pascal <buerger.pascal@web.de>
2020-08-31 15:09:13 +0200

    modify update

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,11 +2,11 @@
 
 echo -e "\e[1mupdate vision systen \e[39m \e[0m"
 
-echo -e "\e[32mstep 1/6: update one-easy-protocol \e[39m"
+echo -e "\e[32mstep 1/7: update one-easy-protocol \e[39m"
 pip3 install --upgrade pip
 pip3 install one-easy-protocol
 
-echo -e "\e[32mstep 2/6: update one_easy_protocol_pkg \e[39m"
+echo -e "\e[32mstep 2/7: update one_easy_protocol_pkg \e[39m"
 echo "pull one_easy_protocol_pkg"
 cd /home/ubuntu/catkin_ws/src/one_easy_protocol_pkg
 git fetch origin
@@ -14,14 +14,14 @@ git reset --hard origin/master
 git pull
 cd /home/ubuntu/catkin_ws/src/one_easy_protocol_pkg/scripts/one_easy_protocol_pkg
 
-echo -e "\e[32mstep 3/6: update visy_detector_pkg \e[39m"
+echo -e "\e[32mstep 3/7: update visy_detector_pkg \e[39m"
 echo "pull visy_detector_pkg"
 cd /home/ubuntu/catkin_ws/src/visy_detector_pkg
 git fetch origin
 git reset --hard origin/master
 git pull
 
-echo -e "\e[32mstep 4/6: update visy_neopixel_pkg \e[39m"
+echo -e "\e[32mstep 4/7: update visy_neopixel_pkg \e[39m"
 echo "pull visy_neopixel_protocol_pkg"
 cd /home/ubuntu/catkin_ws/src/visy_neopixel_pkg
 git fetch origin
@@ -29,7 +29,7 @@ git reset --hard origin/master
 git pull
 cd /home/ubuntu/catkin_ws/src/visy_neopixel_pkg/scripts/visy_neopixel_pkg
 
-echo -e "\e[32mstep 5/6: update visy_sorting_app_pkg \e[39m"
+echo -e "\e[32mstep 5/7: update visy_sorting_app_pkg \e[39m"
 echo "pull visy_sorting_app_pkg"
 cd /home/ubuntu/catkin_ws/src/visy_sorting_app_pkg
 git fetch origin
@@ -37,14 +37,14 @@ git reset --hard origin/master
 git pull
 cd /home/ubuntu/catkin_ws/src/visy_sorting_app_pkg/scripts/visy_sorting_app_pkg
 
-echo -e "\e[32mstep 5/6: update visy_user_interface_pkg \e[39m"
+echo -e "\e[32mstep 6/7: update visy_user_interface_pkg \e[39m"
 echo "pull visy_user_interface_pkg"
 cd /home/ubuntu/catkin_ws/src/visy_user_interface_pkg
 git fetch origin
 git reset --hard origin/master
 git pull
 
-echo -e "\e[32mstep 6/6: build workspace \e[39m"
+echo -e "\e[32mstep 7/7: build workspace \e[39m"
 cd /home/ubuntu/catkin_ws
 catkin build -j1
 sleep  3
