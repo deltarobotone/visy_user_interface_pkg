f80df28aee45d671ea9e8e36ccc843a7b3e5b3af
buerger-pascal <buerger.pascal@web.de>
2020-08-31 16:30:59 +0200

    try without chmod

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,7 +2,7 @@
 
 echo -e "\e[1mupdate vision systen \e[39m \e[0m"
 
-sudo echo -e "\e[32mstep 1/7: update one-easy-protocol \e[39m"
+echo -e "\e[32mstep 1/7: update one-easy-protocol \e[39m"
 pip3 install --upgrade pip
 pip3 install one-easy-protocol
 
@@ -12,9 +12,6 @@ cd /home/ubuntu/catkin_ws/src/one_easy_protocol_pkg
 git fetch origin
 git reset --hard origin/master
 git pull
-cd /home/ubuntu/catkin_ws/src/one_easy_protocol_pkg/scripts/one_easy_protocol_pkg
-sudo chmod +x one_ctrl_node.py
-sudo chmod +x __init__.py
 
 echo -e "\e[32mstep 3/7: update visy_detector_pkg \e[39m"
 echo "pull visy_detector_pkg"
@@ -29,11 +26,6 @@ cd /home/ubuntu/catkin_ws/src/visy_neopixel_pkg
 git fetch origin
 git reset --hard origin/master
 git pull
-cd /home/ubuntu/catkin_ws/src/visy_neopixel_pkg/scripts/visy_neopixel_pkg
-sudo chmod +x light_ring_node.py
-sudo chmod +x neo_pixel_node.py
-sudo chmod +x status_bar_node.py
-sudo chmod +x __init__.py
 
 echo -e "\e[32mstep 5/7: update visy_sorting_app_pkg \e[39m"
 echo "pull visy_sorting_app_pkg"
@@ -41,11 +33,6 @@ cd /home/ubuntu/catkin_ws/src/visy_sorting_app_pkg
 git fetch origin
 git reset --hard origin/master
 git pull
-cd /home/ubuntu/catkin_ws/src/visy_sorting_app_pkg/scripts/visy_sorting_app_pkg
-sudo chmod +x sorting_app_node.py
-sudo chmod +x pick_and_place_node.py
-sudo chmod +x grasp_planner_node.py
-sudo chmod +x __init__.py
 
 echo -e "\e[32mstep 6/7: update visy_user_interface_pkg \e[39m"
 echo "pull visy_user_interface_pkg"
